576fd4bf92b9274cb53170b0204bc0fd0dccd656
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-22 16:31:52 +0100

    file 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Script that runs a flask application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function that return " hello hbnb" """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ function that displays hbnb!"""
+    return 'HBNB'
+
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ function that returns "C" followed by the value of the text"""
+    return 'C {}'.format(text.replace('_', ''))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
